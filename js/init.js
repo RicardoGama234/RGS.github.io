97d5df021a4be5bfdc95d454e49b7e6297d9aff1
RicardoGama234 <RicardoGama234@gmail.com>
2021-04-13 22:02:41 -0500

    Subiendo archivos

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyD42HaYQ3xxe-R9DyuGfHL5szIXdA1RX6I",
+    authDomain: "evaluacion3kbgv.firebaseapp.com",
+    projectId: "evaluacion3kbgv",
+    storageBucket: "evaluacion3kbgv.appspot.com",
+    messagingSenderId: "268080829131",
+    appId: "1:268080829131:web:fe08b78f03cdbbc1e4014e",
+    measurementId: "G-SBJDHLB1EG"
+});
